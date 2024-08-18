6cd4c1083353bd1e652e2f35fe16de389ca754d4
Omar Ellaban <omar.ellaban00@gmail.com>
2024-08-19 02:16:46 +0300

    Some Messages to add uniqe UX

---
+++ Backend/APIsControllers.py
@@ -0,0 +1,21 @@
+# API's Controllers is a magician to give user a unique Exp !
+# UFM - User Friendly Messages will be stored here 
+# You can be kind and add some cute messages here :)
+# NEOs Descriptions will be destriputed in here 
+# User can add fingerprint to last in our lovely project .
+# NEOs Is A Magic Made By God , Care About It .
+
+
+# UFM will be saved with key of WFM - Website Friendly Message . {'BADGE ADDED' : [a list of messages !]}
+UFMs = {
+   'USER_NOT_FOUND' : [
+                        'We think that your login session has been ended',
+                        'This is our fault you can pet a cat until we fix it',
+                        'Security says, this way is under construction, go fill your car tank and get back later',
+                        'Always , always and always it\'s front-end fault :('
+                        'Try again, login again. huh ?',
+                        'Astronuts sleeping now, we can\'t take off, be back later ..',
+                        'Your Ship stolen ! we are getting it in 10 mins',
+                        'You can call it an error, somthing like magic . but we will make this disappear soon ;)'
+                        ] 
+}
