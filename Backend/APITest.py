b9908ef5ce8bccfbd2dcfbad62060da40bdf4efb
Omar Ellaban <113574311+TheBruteforcer@users.noreply.github.com>
2024-09-05 11:45:30 +0300

    Convert to GOlang

--- Backend/APITest.py
+++
@@ -1,3 +0,0 @@
-from requests import get, post
-from json import dumps
-print(post('http://127.0.0.1:8000/CustomExecution/', params={'execution' : dumps({'block_1' : {'head' : 'EXIDB', 'query' : 'SELECT * FROM USERS WHERE USERNAME = "omar"'}})}).json())
