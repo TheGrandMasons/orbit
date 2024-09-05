fa9581ff9413306aa994bcd29e1347a23ffaf87b
Omar Ellaban <113574311+TheBruteforcer@users.noreply.github.com>
2024-09-05 12:07:51 +0300

    Response Messages Structs .

--- Backend/structs/structs.go
+++ Backend/structs/structs.go
@@ -1,1 +1,28 @@
 package structs
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	WFM       string
+	UFM       string
+	Fetched   map[string]interface{}
+	Additions map[string]interface{}
+	FuncState bool // True done, False not.
+}
+
+func (r *Response) JSONifyResponse(w http.ResponseWriter) error {
+	resp := map[string]interface{}{
+		"WFM":       r.WFM,
+		"UFM":       r.UFM,
+		"Fetched":   r.Fetched,
+		"Additions": r.Additions,
+		"FS":        r.FuncState,
+	}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		return err
+	}
+	return nil
+}
