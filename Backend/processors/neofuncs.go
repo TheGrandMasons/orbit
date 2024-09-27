bbb9226782b6fcc17891e50eb9055ea81632f348
Omar Ellaban <113574311+TheBruteforcer@users.noreply.github.com>
2024-09-27 17:03:03 +0300

    Update neofuncs.go

--- Backend/processors/neofuncs.go
+++ Backend/processors/neofuncs.go
@@ -1,5 +1,26 @@
 package neofuncs
+import (
+	structures "main/structs"
+	"net/http"
+	"gorm.io/gorm"
+	"encoding/json"
+       )
 
-func GetObjectInfo() {
-	return
+
+
+func GetObjectInfo(w http.ResponseWriter, r *http.Request, db *gorm.DB) {
+	defer r.Body.Close()
+	// ["id / Name"] -> db > .First(&struct.NEO, {"id"})
+	// struct.Response{Fetched : JSON>NEO}
+	// for range [ENO]/
+	// return JSON{reponse {NEO}}/
+	// if err := json.NewEncoder(w).Encode(response); err != nil {print ...}
+}
+
+func AddObject(w http.ResponseWriter, r *http.Request, db *gorm.DB){
+	// defer r.Body.Close()
+	// var data map[string]interface{}
+	// if err := json.NewDecoder().Decode(r.Body); err != nil {
+	// }
+	// struct.NEO{name data["name"].(string), moons.(float64).(uint)}
 }
