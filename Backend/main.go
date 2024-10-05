19f8dcf62cde96a846b944fe42bd1a409c409edf
Seif <62963968+seifzellaban@users.noreply.github.com>
2024-10-05 10:52:36 +0300

    Update main.go

--- Backend/main.go
+++ Backend/main.go
@@ -12,6 +12,7 @@ func main() {
 	db, err := gorm.Open(sqlite.Open("orbit_data.db"), &gorm.Config{})
 	if err != nil {
 		fmt.Println("Running Error . Error ['Cannot open database'].")
+		fmt.Println("Mango Juice")
 		return
 	}
 	db.AutoMigrate(&structures.User{}, &structures.Favourites{}, &structures.NEO{}, &structures.Visit{})
